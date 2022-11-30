7c3b0be45345ffc2a9d064a81e94f14eaee362f4
francisco perez <frperez032219@gmail.com>
2022-11-30 05:57:11 -0800

    First Commit

---
+++ Cool Beans.md
@@ -0,0 +1,5 @@
+# Cool Beans
+  [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)}
+
+  ## Description
+Auto Genereate a README
---
+++ index.js
@@ -0,0 +1,52 @@
+// TODO: Include packages needed for this application
+const inquirer = require('./node_modules/inquirer');
+
+const generateMarkdown = require('./utils/generateMarkdown')
+
+const fs = require('fs');
+// TODO: Create an array of questions for user input
+const questions = [
+   {
+        type: "input",
+        message: "What do you want your README file to be named?",
+        name: "fileName"
+   },
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is your project about?",
+        name: "description"
+    },
+    {
+    type: "list",
+    message: "What kind of licence is your project?",
+    name: "license",
+    choices: [
+        {name: 'MIT', value:"[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"},
+        {name: 'CC0', value:"[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)"},
+        {name: 'IBM', value:"[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"},
+        {name: 'ISC', value:"[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"},
+        ]
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then(response => {
+            const md = generateMarkdown(response)
+            writeToFile(`${response.fileName}.md`, md)
+        })
+}
+
+// Function call to initialize app
+init();
